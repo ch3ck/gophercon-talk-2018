2ce5f900915fc9354ca2c305deda9d8cc1d832eb
Nyah Check <4006891+ch3ck@users.noreply.github.com>
2018-08-28 13:43:11 -0600

    Add mistake 3

--- 03-pointer-in-non-visible-slice-portion/examples/fixedSlice.go
+++ 03-pointer-in-non-visible-slice-portion/examples/fixedSlice.go
@@ -8,11 +8,11 @@ import (
 func main() {
 	a := []*int{new(int), new(int)}
 	fmt.Println(a)
-	b := a[:1]
-	fmt.Println(b) // [&0]
 
-	// second element is not garbage collected, because it's *still* accessible
-	c := b[:2] //[&0[
+	// Using the 3- index slice operation
+	b := a[:1:1]
+	fmt.Println(b)
+	c := b[:2]
 	fmt.Println(c)
 }
 
