1d487806d8e4ad3b26b19a4dd78723310f84643b
Nyah Check <4006891+ch3ck@users.noreply.github.com>
2018-08-27 23:20:22 -0600

    Add example code for mistake 5

---
+++ 05-errors-are-not-strings/examples/errors.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/pkg/errors"
+)
+
+func connect(addr string) error {
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		switch err := err.(type) {
+		case *net.OpError:
+			// return fmt.Errorf("failed to connect to %s: %v", err.Net, err)
+			return errors.Wrapf(err, "failed to connect to %s", err.Net)
+		default:
+			// return fmt.Errorf("unkown error: %v", err)
+			return errors.Wrap(err, "unknown error")
+		}
+	}
+
+	defer conn.Close()
+
+	return nil
+}
+
+func main() {
+	if err := connect("goooooooooooogle.com:80"); err != nil {
+		fmt.Printf("%T\n", err)
+		fmt.Printf("%T\n", errors.Cause(err)) //checking what possibly caused this error
+	}
+}
---
+++ 05-errors-are-not-strings/examples/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "goooooooooooogle.com:80")
+	if err != nil {
+		switch err := err.(type) {
+		case *net.OpError:
+			fmt.Println(err)
+			fmt.Printf("failed to connect to %s because %v\n", err.Net, err.Err)
+			return
+		default:
+			log.Fatal(err)
+		}
+	}
+	defer conn.Close()
+}
